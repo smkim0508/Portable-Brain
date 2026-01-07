3b2ccb0698e2f9ba60f58c53929291865724c1ce
Sungmin Kim <sk3378@princeton.edu>
2026-01-08 01:47:48 +0900

    update: fleshed out baseline action types to observe for now

--- src/portable_brain/monitoring/background_tasks/types/actions.py
+++ src/portable_brain/monitoring/background_tasks/types/actions.py
@@ -27,27 +27,71 @@ class ActionSource(str, Enum):
 
 # specific actions, as defined by the types above
 # NOTE: each action inherits shared metadata from ActionBase class
+
+# TODO: for now, experiment with mostly text messages across three applications, then expand later
 class AppSwitchAction(GenericActionBase):
     """
-    User switched between apps.
+    Action for switching between apps.
     """
     type: Literal[GenericActionType.APP_SWITCH] = GenericActionType.APP_SWITCH
-    from_package: str
-    to_package: str
-    from_activity: Optional[str] = None
-    to_activity: Optional[str] = None
+    src_package: str
+    dst_package: str
+    src_activity: Optional[str] = None
+    dst_activity: Optional[str] = None
 
 class InstagramMessageSentAction(InstagramActionBase):
+    """
+    Action for sending text message on Instagram.
+    """
     # NOTE: should log entire stream of messages in a single session, not every single message
     type: Literal[InstagramActionType.MESSAGE_SENT] = InstagramActionType.MESSAGE_SENT
-    sender_username: str
-    recipient_username: str
-    message_summary: str
+    actor_username: str
+    target_username: str
+    message_summary: Optional[str] = None
+
+class InstagramPostLikedAction(InstagramActionBase):
+    """
+    Action for liking a post on Instagram.
+    """
+    type: Literal[InstagramActionType.POST_LIKED] = InstagramActionType.POST_LIKED
+    actor_username: str
+    target_username: str
+    post_description: Optional[str] = None
+
+class WhatsAppMessageSentAction(WhatsAppActionBase):
+    """
+    Action for sending text message on WhatsApp.
+    """
+    # NOTE: should log entire stream of messages in a single session, not every single message
+    type: Literal[WhatsAppActionType.MESSAGE_SENT] = WhatsAppActionType.MESSAGE_SENT
+    actor_name: Optional[str] = None # assumed to have a sole WhatsApp account
+    target_name: str
+    message_summary: Optional[str] = None
+
+class SlackMessageSentAction(SlackActionBase):
+    """
+    Action for sending text message on Slack.
+    """
+    # NOTE: should log entire stream of messages in a single session, not every single message
+    type: Literal[SlackActionType.MESSAGE_SENT] = SlackActionType.MESSAGE_SENT
+    workspace_name: str
+    channel_name: str
+    thread_name: Optional[str] = None
+    is_dm: bool = False # default assumed to be false
+    target_name: Optional[str] = None # only if message is a dm
+    message_summary: Optional[str] = None
 
 # NOTE: uses Union to represent all possible action types without losing Pydantic schema
 Action = Union[
+    # Generic actions
     AppSwitchAction,
-    InstagramMessageSentAction
+    # Instagram actions
+    InstagramMessageSentAction,
+    InstagramPostLikedAction,
+    # WhatsApp actions
+    WhatsAppMessageSentAction,
+    # Slack actions
+    SlackMessageSentAction
     # more to be added...
 ]
 
